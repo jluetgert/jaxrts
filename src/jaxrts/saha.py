f1b34bcc72b8e176b6774333ff58ff7faab83aa1
lue <julian.luetgert@uni-rostock.de>
2026-03-06 10:06:23 +0100

    BU solver

--- src/jaxrts/saha.py
+++ src/jaxrts/saha.py
@@ -18,11 +18,32 @@ from .plasma_physics import (
     therm_de_broglie_wl,
 )
 from .units import Quantity, to_array, ureg
-from .helpers import bisection
+from .helpers import bisection, read_nist_file
 
 h = 1 * ureg.planck_constant
 k_B = 1 * ureg.boltzmann_constant
 m_e = 1 * ureg.electron_mass
+
+
+@jax.jit
+def planck_larkian_partition_sum(
+    g: jnp.ndarray, E: Quantity, T: Quantity
+) -> jnp.ndarray:
+    """
+    See :cite:`Kramp.2005` eqn 6.182, with a cutoff for negative energies
+    """
+    ratio = (E / (T * ureg.boltzmann_constant)).m_as(ureg.dimensionless)
+    return jnp.sum(g * (jnp.exp(-ratio) - 1 + ratio) * jnp.heaviside(ratio, 0))
+
+
+def gen_balance_equation(
+    part_i: float | jnp.ndarray,
+    part_j: float | jnp.ndarray,
+    T_e: Quantity,
+    n_e: Quantity,
+    chem_pot: Quantity,
+) -> Quantity:
+    return (part_j / part_i) * n_e * jnpu.exp(-chem_pot / (1 * k_B * T_e))
 
 
 @jax.jit
@@ -620,6 +641,275 @@ def solve_gen_saha(
             skip += element.Z + 1
 
         _diag = jnp.diag(ne_line, -1)
+
+        out = M + _diag
+        out = out.at[-1, -1].set(ne)
+        # Transpose
+        return out.T
+
+    def det_M(M, ne):
+        res = jnp.linalg.det(insert_ne(M, ne))
+        return res
+
+        # Find n_e by finding the root where the determinant of M is 0
+        # Use the bisection method, boundaries are fixed by max_ne and 0.
+
+    def det_M_func(ne):
+        return det_M(M=M, ne=ne)  # * 1E-10
+
+    sol_ne, iterations = bisection(
+        jax.tree_util.Partial(det_M_func),
+        0,
+        (max_ne / ne_scale).m_as(ureg.dimensionless),
+        tolerance=1e-16,
+        max_iter=1000,
+        min_iter=1,
+    )
+
+    # Create the matrix that describes the linear system of equations we solve.
+    # Insert n_e.
+    concrete_M = insert_ne(M, sol_ne)
+
+    # Strip the last row and column. Use the latter as inhomogeneity.
+    # M1 = concrete_M[: (len(concrete_M[0]) - 1), 0 : (len(concrete_M[0]) - 1)]
+    # M2 = concrete_M[: (len(concrete_M[0]) - 1), (len(concrete_M[0]) - 1)]
+
+    M1 = concrete_M[:-1, :-1]
+    M2 = concrete_M[:-1, -1]
+
+    # Get the solution of the set of linear equations of the form
+    # (nIon0_0+,nIon0_1+, ..., nIon1_0+,nIon1_1+, ...)
+
+    ionised_number_densities = jnp.linalg.solve(M1, M2)
+
+    # Rescale the dimensionless number densities so that units are
+    # 1/[length]**3.
+
+    res = jnpu.multiply(ionised_number_densities, ne_scale)
+
+    skip = 0
+    Z_mean = []
+    for element in element_list:
+        Z_mean.append(
+            jnpu.sum(
+                res[skip : skip + element.Z + 1]
+                * jnp.arange(element.Z + 1)
+                / jnpu.sum(res[skip : skip + element.Z + 1])
+            )
+        )
+        skip += element.Z + 1
+
+    return (
+        to_array(res),
+        sol_ne * ne_scale,
+        to_array(Z_mean).m_as(ureg.dimensionless),
+    )
+
+
+@partial(jax.jit, static_argnames=["element_list"])
+def solve_BU(
+    element_list: list[Element],
+    T_e: Quantity,
+    n_e: Quantity,
+    ion_number_densities: Quantity,
+    continuum_lowering: Quantity = 0 * ureg.electron_volt,
+    chem_pot_ideal: Quantity = 0 * ureg.electron_volt,
+) -> (Quantity, Quantity):
+    """
+    Solve the BU equation for a list of elements at a given temperature.
+
+    This function uses a similar approach to solve the set of equations as
+    Jamal El Kuweiss' `many-ion-saha-equation tool
+    <https://github.com/jelkuweiss/many-ion-saha-equation>`_
+    by
+
+    #. Creating an abstract matrix which for each element consists of
+
+        #. `Z + 1` rows of which have the solution of the BU equation as
+           diagonal entries, multiplied by -i and the free electron density
+           `n_e` on the first off-diagonal element. These rows reflect that
+           every neighboring number densities `n_{i+1}` and `n_i` fulfill
+           the BU equation
+
+        #. A row with `Z + 1` entries of 1, ending with the diagonal entry,
+           and the ion number density as last entry of the row. This guarantees
+           that the individual ionization number densities add up to the full
+           ion number density.
+
+       And additionally a final row, which contains the possible charge
+       states for all ions, respectively, and, finally, the free electron
+       number density n_e.
+       For all not-specified entries, this matrix is sparse:
+
+       .. image:: ../images/SahaMatrix.svg
+
+       Throughout the functions, densities are converted to non-dimensional
+       quantities. The scale has impact in numerical stability.
+
+    #. Finding the correct free electron density, which is given by the root
+       of the determinant of the matrix.
+
+    #. Building a concrete matrix by inserting the free electron density
+
+    #. Finding the number densities for all ionization levels by solving the
+       remaining set of equations by stripping the last row and column of the
+       matrix, where the latter is the in-homogeneity of the set of
+       equations.
+
+    The ionization energies are taken from the provided
+    :py:class:`jaxrts.element.Element`, but we allow for a modification in form
+    of continuum_lowering, which is an optional parameter.
+
+    Parameters
+    ----------
+    element_list
+        A list of :py:class:`jaxrts.element.Element`.
+    T_e
+        The electron temperature of the plasma.
+    ion_number_densities
+        A list of number densities for the individual ions. Has to have the
+        same size as `element_list`.
+    continuum_lowering: Quantity, default: 0 eV
+        A fixed value that is subtracted from all binding energies. Defaults to
+        0 eV.
+    chem_pot_ideal: Quantity, default: 0 eV
+        The ideal chemical potential used in the BU equation.
+
+    Returns
+    -------
+    ionised_number_densities: Quantity
+        A jax.numpy.ndarray of number densities per ionization state. It is
+        ordered per-ion-species, and then with increasing ionization degree.
+        I.e., `[nIon0 0+, nIon0 1+, ..., nIon1 0+, nion1 1+, ...]`.
+    n_e: Quantity
+        The free electron number density.
+    Z_mean:  Quantity
+        The mean free charge of each ion in the plasma.
+
+    See Also
+    --------
+    jaxrts.saha.gen_balance_equation
+         Function used to calculate balance between two adjacent charge states.
+    jaxrts.saha.planck_larkian_partition_sum
+         The partition sum approximation used.
+    """
+
+    Z = [i.Z for i in element_list]
+    if continuum_lowering is None:
+        continuum_lowering = [
+            jnp.zeros(ion.Z) * ureg.electron_volt for ion in element_list
+        ]
+
+    # Set up the empty matrix
+    M = jnp.zeros(
+        (
+            len(element_list) + int(onp.sum(Z)) + 1,
+            len(element_list) + int(onp.sum(Z)) + 1,
+        )
+    )
+
+    # Maximal electron number density per element, if fully ionized -> Sum up
+    # for maximal full electron number density
+    max_ne = jnpu.sum(jnp.array(Z) * ion_number_densities)
+
+    # Interpolate between a small ne and the maximal ne, depending on the
+    # temperature to avoid numerical instabilities
+    part_funcs = []
+    for element, ipd in zip(element_list, continuum_lowering, strict=True):
+        pls = []
+        for charge, cl in zip(range(element.Z), ipd, strict=True):
+            g, E = read_nist_file(f"{element.symbol}{charge}")
+            E += cl
+            pls.append(planck_larkian_partition_sum(g, E, T_e))
+        pls.append(1)
+        part_funcs.append(jnp.array(pls))
+
+    all_part_funcs = jnp.concatenate(part_funcs)
+
+    nom = jnp.heaviside(all_part_funcs, 0)
+    ratio = jnp.sum(nom / len(all_part_funcs))
+
+    _ne_range = jnp.array(
+        [
+            max_ne.m_as(1 / ureg.m**3) * (1 - ratio) + 1,
+            max_ne.m_as(1 / ureg.m**3),
+        ]
+    )
+    ne_scale = (
+        jnp.interp(
+            (T_e * k_B).m_as(ureg.electron_volt),
+            jnp.array([1, 1000]),
+            _ne_range,
+            left=_ne_range[0],
+            right=_ne_range[-1],
+        )
+        * (1 / ureg.m**3)
+        * 1e2
+    )
+
+    # Offset (each element will have a block of size Z+1) This value specifies
+    # the block.
+    skip = 0
+    ionization_states = []
+
+    # Fill the matrix, without any entries containing n_e, the free electron
+    # number density (the Saha part does contain n_e, but implicitly).
+    # Note: The matrix is transposed, here. It will be transposed when n_e is
+    # inserted.
+    for ion_dens, element, part_func in zip(
+        ion_number_densities, element_list, part_funcs, strict=True
+    ):
+        coeff = (
+            gen_balance_equation(
+                part_func[:-1],
+                part_func[1:],
+                T_e,
+                n_e,
+                chem_pot_ideal,
+            )
+            / ne_scale
+        ).m_as(ureg.dimensionless)
+
+        diag = jnp.diag(jnp.where(part_func[:-1] > 0, (-1) * coeff, 1))
+
+        dens_row = jnp.ones(element.Z + 1)
+
+        # Set the diagonal for the Saha-rows
+        M = M.at[skip : skip + element.Z, skip : skip + element.Z].set(diag)
+        # Create ones for the density row.
+        M = M.at[skip : skip + element.Z + 1, skip + element.Z].set(dens_row)
+        # Set the last element of the density row to the ion number density
+        M = M.at[-1, skip + element.Z].set(
+            (ion_dens / ne_scale).m_as(ureg.dimensionless)
+        )
+
+        # Each element requires a block of Z+1 size.
+        skip += element.Z + 1
+
+        # This is for the final row, which lists all ionization states for the
+        # elements
+        ionization_states += list(jnp.arange(element.Z + 1))
+
+    # Set the row for all ionization states.
+    M = M.at[:-1, -1].set(jnp.array(ionization_states))
+
+    def insert_ne(M, ne):
+        """
+        Add n_e to the off-diagonal end the last entry (last row and column).
+        """
+
+        ne_line = jnp.ones(len(element_list) + int(onp.sum(Z))) * ne
+        skip = -1
+        for element, part_func in zip(element_list, part_funcs, strict=True):
+            # Not the full off-diagonal equals n_e: The density rows don't
+            # contain it!
+            ne_line = ne_line.at[skip + 1 : skip + element.Z + 1].multiply(
+                    jnp.heaviside(part_func[:-1], 0)
+            )
+            ne_line = ne_line.at[skip + element.Z + 1].set(0.0)
+            skip += element.Z + 1
+
+        _diag = jnp.diag(ne_line, -1)
         out = M + _diag
         out = out.at[-1, -1].set(ne)
         # Transpose
@@ -819,5 +1109,91 @@ def calculate_mean_free_charge_saha(
             )
 
             plasma_state.Z_free = jnp.array(Z_mean)
+
+    return charge_distribution, Z_mean
+
+
+def calculate_mean_free_charge_BU(
+    plasma_state,
+    use_ipd: bool = False,
+    use_distribution: bool = False,
+):
+    """
+    Calculates the mean charge of each ion in a plasma using the BU
+    equation.
+
+    Parameters
+    ----------
+    plasma_state : PlasmaState
+        The plasma state object.
+    use_ipd : bool
+        If true, the ipd correction of the plasma state is used to reduce the
+        continuum. Note: the IPD can very much depend on the ionization state.
+        this could result in some circular dependency.
+    use_chem_pot : bool
+        If true, the chemical potential of the plasma state is used, instead of
+        the non-degenerate limiting case.
+    use_distribution:
+        If true, the ipd is evaluated for each ion species separately.
+    exclude_non_negative_energies : bool, default = True
+        If true, bound states for which the ionization energy is pushed into
+        the continuum are removed from the calculation and do not appear with
+        their Boltzmann factors in the BU equations.
+
+    Returns
+    -------
+    jnp.ndarray
+        The charge state distribution in fractions.
+    jnp.ndarray
+        An array containing the mean charge of each ion in the plasma.
+
+    See Also
+    --------
+    jaxrts.saha.solve_BU
+        Function used to solve the BU equation
+    """
+
+    plasma_state.Z_free = jnp.array(
+        jnp.max(jnp.array([i.Z for i in plasma_state.ions]))
+    )
+    chem_pot_ideal = plasma_state["chemical potential"].evaluate(
+        plasma_state, None
+    )
+    cl = [jnp.zeros(ion.Z) * ureg.electron_volt for ion in plasma_state.ions]
+
+    charge_distribution, ne, Z_mean = solve_BU(
+        tuple(plasma_state.ions),
+        plasma_state.T_e,
+        plasma_state.n_e,
+        (plasma_state.mass_density / plasma_state.atomic_masses),
+        continuum_lowering=cl,
+        chem_pot_ideal=chem_pot_ideal,
+    )
+
+    for k in range(6):
+        chem_pot_ideal = plasma_state["chemical potential"].evaluate(
+            plasma_state, None
+        )
+        if use_ipd:
+            cl = plasma_state["ipd"].all_element_states(
+                plasma_state,
+                charge_distribution if use_distribution else None,
+            )
+        else:
+            cl = [
+                jnp.zeros(ion.Z) * ureg.electron_volt
+                for ion in plasma_state.ions
+            ]
+
+        charge_distribution, ne, Z_mean = solve_BU(
+            tuple(plasma_state.ions),
+            plasma_state.T_e,
+            plasma_state.n_e,
+            (plasma_state.mass_density / plasma_state.atomic_masses),
+            continuum_lowering=cl,
+            chem_pot_ideal=chem_pot_ideal,
+        )
+
+        plasma_state.Z_free = jnp.array(Z_mean)
 
     return charge_distribution, Z_mean
